2bcd93cfd45a4f582c090bfb14cb2b5c84abc660
Inayat Yousuf <inayatyousuf77@gmail.com>
2022-08-25 18:15:59 +0530

    fix bug

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+// code for Collapse the navbar when the link is clicked
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
+
 
 $('.a-top-nav').click(function() {
   $('.a-top-nav').removeClass("active-link");
   $('a-top-nav.active').removeClass("active-link");
   $(this).addClass("active-link");
 });
-
 
   
